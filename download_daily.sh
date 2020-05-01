1c46f0e10d790f0bfe9954334861fac91c208a07
Tfangz <tfangz@ymail.com>
2020-05-01 19:42:52 +0800

    Update download_daily.sh

--- download_daily.sh
+++ download_daily.sh
@@ -4,6 +4,9 @@
 #python3 download_daily.py `date -d "$i month ago" '+%Y%m'`
 #done
 
+# 如果没有当月文件，就下载上个月数据。
+date=`date +'%Y%m'`
+[ ! -e "/home/toby/data/datasource/baostock/daily/$date.csv" ] && python3 download_daily.py `date -d '1 month ago' '+%Y%m'`   # 下载上个月
+
 python3 download_daily.py `date +'%Y%m'`   # 下载当月
-python3 download_daily.py `date -d '1 month ago' '+%Y%m'`   # 下载上个月
 
