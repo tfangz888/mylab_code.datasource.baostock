0ef4cb6a8ee398f8069e2e37ea611064091f6e9a
Tfangz <tfangz@ymail.com>
2020-05-01 18:33:07 +0800

    Update download_daily.sh

--- download_daily.sh
+++ download_daily.sh
@@ -1,2 +1,8 @@
+#for((i=172;i<=244;i++));  
+#do   
+## echo `date -d "$i month ago" '+%Y%m'`
+#python3 download_daily.py `date -d "$i month ago" '+%Y%m'`
+#done
+
 python3 download_daily.py `date -d '1 month ago' '+%Y%m'`   # 下载上个月
 python3 download_daily.py `date +'%Y%m'`   # 下载当月
